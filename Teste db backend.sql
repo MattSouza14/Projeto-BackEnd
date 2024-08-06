f011d48cbfa61e5aaa5885b1ea85a016fe7857c4
MATEUS SOUZA DO NASCIMENTO <MATEUS6268@DIGITALCOLLEGE.LOCAL>
2024-08-06 16:16:17 -0300

    Atualização na estrutura do db

--- Teste db backend.sql
+++ Teste db backend.sql
@@ -24,18 +24,27 @@ CREATE TABLE categories(
     use_in_menu BOOL
     );
 
-
 CREATE TABLE imagesProducts(
 	id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     product_id INTEGER,
-    FOREIGN KEY (productID) REFERENCES products(id),
+    FOREIGN KEY (product_id) REFERENCES products(id),
     enabled BOOL,
     pathProduct VARCHAR(150) NOT NULL
 );
+CREATE TABLE option_products(
+id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+product_id INT NOT NULL,
+title VARCHAR(100) NOT NULL,
+shape ENUM ('square', 'circle') DEFAULT 'square',
+`type` ENUM('text','collor') DEFAULT 'text',
+`values`VARCHAR(100) NOT NULL,
+FOREIGN KEY (product_id) REFERENCES products(id)
+);
 
 CREATE TABLE categories_product(
-	product_id INTEGER NOT NULL,
-    category_id INTEGER NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-    );
+product_id INTEGER NOT NULL,
+category_id INTEGER NOT NULL,
+PRIMARY KEY (product_id, category_id),
+FOREIGN KEY (product_id) REFERENCES products(id), 
+FOREIGN KEY (category_id) REFERENCES categories(id)
+);
