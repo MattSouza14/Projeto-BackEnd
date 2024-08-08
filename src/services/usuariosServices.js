ab510138c53f49b94424344fb1dca9e9372ee144
MattSouza14 <mateussouzabr1@gmail.com>
2024-08-08 17:07:48 -0300

    Criação da função create user com bcrypt

--- src/services/usuariosServices.js
+++ src/services/usuariosServices.js
@@ -26,73 +26,79 @@ const getUsuario = async (req, res) => {
       }
   }
 
-  const createUsuario1 = async (req, res) => {
-    const { userName, surName, email, password, userAtivo, dataCadastro } = Usuario.req.body
-    try {
-      const salt = await bcrypt.genSalt(10)
-      const hashedSenha = await bcrypt.hash(password, salt)
-      const novoUsuario = await Usuario.create({ 
-        userName: userName, 
-        surName: surName, 
-        userAtivo: userAtivo,
-        email: email, 
-        password: hashedSenha,
-        dataCadastro:dataCadastro
+  // const createUsuario1 = async (req, res) => {
+  //   const { userName, surName, email, password, userAtivo, dataCadastro } = Usuario.req.body
+  //   try {
+  //     const salt = await bcrypt.genSalt(10)
+  //     const hashedSenha = await bcrypt.hash(password, salt)
+  //     const novoUsuario = await Usuario.create({ 
+  //       userName: userName, 
+  //       surName: surName, 
+  //       userAtivo: userAtivo,
+  //       email: email, 
+  //       password: hashedSenha,
+  //       dataCadastro:dataCadastro
 
-       })
-      const usuarioExistente = await novoUsuario.findOne({ where: { email } })
-      if (usuarioExistente) {
-        return res.status(400).json({ 
-          statusCode: 400,
-          error: 'Email já está em uso.' 
-        })
+  //      })
+  //     const usuarioExistente = await novoUsuario.findOne({ where: { email } })
+  //     if (usuarioExistente) {
+  //       return res.status(400).json({ 
+  //         statusCode: 400,
+  //         error: 'Email já está em uso.' 
+  //       })
         
-      }
-      let createSucess ={
-        statusCode: 201,
-        firstname: novoUsuario.userName,
-        surname: novoUsuario.surName,
-        email: novoUsuario.email,
-        password: novoUsuario.password,
+  //     }
+  //     let createSucess ={
+  //       statusCode: 201,
+  //       firstname: novoUsuario.userName,
+  //       surname: novoUsuario.surName,
+  //       email: novoUsuario.email,
+  //       password: novoUsuario.password,
         
-      }
-      res.status(201).json(createSucess)
-    } catch (error) {
-      res.status(400).json({ 
-        statusCode: 400,
-        error: 'Erro ao criar usuário.'
-       })
-    }
-  }
+  //     }
+  //     res.status(201).json(createSucess)
+  //   } catch (error) {
+  //     res.status(400).json({ 
+  //       statusCode: 400,
+  //       error: 'Erro ao criar usuário.'
+  //      })
+  //   }
+  // }
   const createUsuario = async (req, res) => {
-  const { userName, surName, userAtivo, email, senha,dataCadastro } = req.body;
+    const { userName, surName, userAtivo, email, password, dataCadastro} = Usuario;
     const saltRounds = 10;
-    bcrypt.hash(senha, saltRounds)
-        .then(senhaCriptografada => {
-            return Usuario.create({
-              userName: userName, 
-              surName: surName, 
-              userAtivo: userAtivo,
-              email: email, 
-              password: senhaCriptografada,
-              dataCadastro:dataCadastro
-            });
-        })
-        .then(gato => {
-            res.json({
-                message: 'Gato criado com sucesso',
-                gato: gato
-            });
-        })
-        .catch(erro => {
-            console.log(erro);
-            res.json({
-                message: 'Erro ao criar gato'
-            });
-        });
-}
-  
-
+      bcrypt.hash(password, saltRounds)
+          .then(senhaCriptografada => {
+              return Usuario.create({
+                userName: userName, 
+                surName: surName, 
+                userAtivo: userAtivo,
+                email: email, 
+                password: senhaCriptografada,
+                dataCadastro:dataCadastro,
+                createAt: createdAt,
+                updateAt: updatedAt
+              });
+          })
+          .then(user => {
+            let createSucess ={
+              statusCode: 201,
+              firstname: user.userName,
+              surname: user.surName,
+              email: user.email,
+              password: user.password,
+            }
+             res.status(201).json(createSucess);
+          })
+          .catch(erro => {
+              console.log(erro);
+              res.status(400).json({
+                  statusCode: 400,
+                  message: 'Erro ao criar um novo usuario'
+              });
+          });
+  }
+    
 // const uptadeUsuario = async (req, res)=>{
 //     try{
 
