1af848ba0f6348584b22f9ff5f12f956538c6a1f
MattSouza14 <mateussouzabr1@gmail.com>
2024-08-18 16:00:25 -0300

    Alteração para teste de deploy no render

--- src/config/database.js
+++ src/config/database.js
@@ -4,15 +4,22 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = new Sequelize(process.env.DB_NAME,
-   process.env.DB_USER,
-   process.env.DB_PASS, 
-	{
-	  host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-	  dialect: 'mysql',
-	}
+const sequelize = new Sequelize('railway', 'root', 'DdcFmollntQRrIVsPPmeNIvqasmEpMYt', 
+ {
+   host: 'viaduct.proxy.rlwy.net',
+   port: '12970',
+   dialect: 'mysql',
+ }
 );
+// const sequelize = new Sequelize(process.env.DB_NAME,
+//    process.env.DB_USER,
+//    process.env.DB_PASS, 
+// 	{
+// 	  host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+// 	  dialect: 'mysql',
+// 	}
+// );
 
 sequelize.authenticate()
   .then(() => {
