73dbe66d6c8d4b42326345e74421d11162d4430a
MattSouza14 <mateussouzabr1@gmail.com>
2024-08-18 15:26:48 -0300

    teste para depoy de api

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,7 +5,7 @@ const productController = require('../controllers/productControllers');
 router.get('/:id', productController.getProduct)
 router.get('/product', productController.getProduct);
 router.post('/createproduct', productController.createProduct);
-//router.delete('/product/:id', productController.deleteProduct)
+router.delete('/product/:id', productController.deleteProduct)
 
 
 
